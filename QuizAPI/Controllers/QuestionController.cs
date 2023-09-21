33c3d24710eea236c09b1d2fb5739d5d195ae6a2
rovinsharmagit <internetsharma45@gmail.com>
2023-09-21 18:20:38 +0530

    Add project files.

---
+++ QuizAPI/Controllers/QuestionController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+using QuizAPI._dbContext;
+
+namespace QuizAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly QuizDbContext _context;
+
+        public QuestionController(QuizDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Question
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Questions>>> GetQuestions()
+        {
+          if (_context.Questions == null)
+          {
+              return NotFound();
+          }
+            return await _context.Questions.ToListAsync();
+        }
+
+        // GET: api/Question/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Questions>> GetQuestions(int id)
+        {
+          if (_context.Questions == null)
+          {
+              return NotFound();
+          }
+            var questions = await _context.Questions.FindAsync(id);
+
+            if (questions == null)
+            {
+                return NotFound();
+            }
+
+            return questions;
+        }
+
+        // PUT: api/Question/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutQuestions(int id, Questions questions)
+        {
+            if (id != questions.QnId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(questions).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!QuestionsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Question
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Questions>> PostQuestions(Questions questions)
+        {
+          if (_context.Questions == null)
+          {
+              return Problem("Entity set 'QuizDbContext.Questions'  is null.");
+          }
+            _context.Questions.Add(questions);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetQuestions", new { id = questions.QnId }, questions);
+        }
+
+        // DELETE: api/Question/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuestions(int id)
+        {
+            if (_context.Questions == null)
+            {
+                return NotFound();
+            }
+            var questions = await _context.Questions.FindAsync(id);
+            if (questions == null)
+            {
+                return NotFound();
+            }
+
+            _context.Questions.Remove(questions);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool QuestionsExists(int id)
+        {
+            return (_context.Questions?.Any(e => e.QnId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ QuizAPI/_dbContext/QuizDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizAPI.Models;
+
+namespace QuizAPI._dbContext
+{
+    public class QuizDbContext : DbContext
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options)
+        { }
+
+        public DbSet<Questions> Questions { get; set; }
+        public DbSet<Participants> Participants { get; set; }
+    }
+}
